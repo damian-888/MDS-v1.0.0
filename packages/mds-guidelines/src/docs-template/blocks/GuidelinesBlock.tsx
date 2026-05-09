d427d9488925156f48d278eb6ac37282d33652ee
Damian <damian@DamianClarkMBP.fritz.box>
2026-05-09 09:20:03 +0200

    refactor(@mds/guidelines): GuidelinesBlock uses .mds-guidelines* classes

--- packages/mds-guidelines/src/docs-template/blocks/GuidelinesBlock.tsx
+++ packages/mds-guidelines/src/docs-template/blocks/GuidelinesBlock.tsx
@@ -1,38 +1,6 @@
 import { useOf } from '@storybook/addon-docs/blocks';
-import type { CSSProperties } from 'react';
 import { MDSIcon } from '@mds/components';
 import type { MDSDocsParams } from '../types';
-
-const headingStyle: CSSProperties = {
-  display: 'flex',
-  alignItems: 'center',
-  gap: 'var(--mds-spacing-small)',
-  margin: 0,
-  fontFamily: 'var(--mds-typography-h3-font-family)',
-  fontSize: 'var(--mds-typography-h3-font-size)',
-  lineHeight: 'var(--mds-typography-h3-line-height)',
-  fontWeight: 'var(--mds-typography-h3-font-weight)' as CSSProperties['fontWeight'],
-  color: 'var(--mds-colours-basic-text)',
-};
-
-const iconStyle: CSSProperties = {
-  color: 'var(--mds-colours-basic-accent)',
-  flexShrink: 0,
-};
-
-const listStyle: CSSProperties = {
-  marginTop: 'var(--mds-spacing-small)',
-  marginBottom: 0,
-  paddingLeft: 'var(--mds-spacing-medium)',
-  fontFamily: 'var(--mds-typography-body-font-family)',
-  fontSize: 'var(--mds-typography-body-font-size)',
-  lineHeight: 'var(--mds-typography-body-line-height)',
-  color: 'var(--mds-colours-basic-text)',
-};
-
-const listItemStyle: CSSProperties = {
-  marginTop: 'var(--mds-spacing-small)',
-};
 
 export function GuidelinesBlock() {
   const resolved = useOf('meta', ['meta']);
@@ -42,37 +10,26 @@ export function GuidelinesBlock() {
   if (!guidelines) return null;
 
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr',
-        gap: 'var(--mds-spacing-extra-large)',
-        marginTop: 0,
-      }}
-    >
+    <div className="mds-guidelines">
       <section>
-        <h4 style={headingStyle}>
-          <MDSIcon name="check" size="small" aria-hidden style={iconStyle} />
+        <h4 className="mds-guidelines-heading">
+          <MDSIcon name="check" size="small" aria-hidden className="mds-guidelines-icon" />
           When to use
         </h4>
-        <ul style={listStyle}>
+        <ul className="mds-guidelines-list">
           {guidelines.use.map((line) => (
-            <li key={line} style={listItemStyle}>
-              {line}
-            </li>
+            <li key={line}>{line}</li>
           ))}
         </ul>
       </section>
       <section>
-        <h4 style={headingStyle}>
-          <MDSIcon name="x" size="small" aria-hidden style={iconStyle} />
+        <h4 className="mds-guidelines-heading">
+          <MDSIcon name="x" size="small" aria-hidden className="mds-guidelines-icon" />
           When NOT to use
         </h4>
-        <ul style={listStyle}>
+        <ul className="mds-guidelines-list">
           {guidelines.avoid.map((line) => (
-            <li key={line} style={listItemStyle}>
-              {line}
-            </li>
+            <li key={line}>{line}</li>
           ))}
         </ul>
       </section>
